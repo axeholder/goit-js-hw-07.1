b4746519fbedd812ea095a4ab10987946ac9abe9
Mykhailo Sokyrko <sokirko.mm@gmail.com>
2023-05-07 22:58:51 +0300

    gallery.js upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  const markup = galleryItems.map((item) => {
+    return `<li class="gallery__item"> 
+        <a class = "gallery__link" 
+            href = "large-image.jpg">
+        <img class = "gallery__image"
+            src = "small-image.jpg"
+            data - source = "large-image.jpg"
+            alt = "Image description" />
+        </a>{" "} 
+        </li>`;
+  });
+  console.log(markup);
+}
+console.log(createGalleryMarkup(galleryItems));
